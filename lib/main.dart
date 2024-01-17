e0c6fdd82ae4e3f14398ccc2fccd057077cf65e3
FLUTTER <miryamsam48@gmail.com>
2024-01-17 00:19:26 -0800

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+
         appBar: AppBar(
           title: Text('Firebase'),
           centerTitle: true,
